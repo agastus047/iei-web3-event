abf61a96d675ececfd6923314f446b046827fc7c
Agastus Joyson <agastusjoyson@gmail.com>
2023-02-11 15:44:46 +0530

    Add style

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}","./dist/index.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['Work Sans','sans-serif'],
+      sourcesans: ['Source Sans Pro','sans-serif'],
+    }
+  },
+  plugins: [],
+}
+
